15eba2eb3b4ff9935b82378fc90c20b886591769
Yuchuan <yyc478@gmail.com>
2017-03-14 00:20:26 +0800

    Add stubs for frontend development

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis");
 
-/* GET home page. */
+var db = redis.createClient();
+
+db.on('error', function(err){
+  console.log("error")
+})
+
+// Advay: Here are stubs for you to test the layout
+
+/* Home page: Leaderboard */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Leaderboard',
+    leaders: [
+      {name: "Ary", time: 10.2},
+      {name: "Hailong", time: 12.2},
+      {name: "Advay", time: 15.9},
+      {name: "Jiayee", time: 20.2319290},
+      {name: "Herbert Illhan Tanujaya", time: 23},
+      {name: "Govind", time: 50.99},
+      {name: "yyc", time: 60.709},
+      {name: "Shiyuan", time: 60.709},
+    ]
+  });
+});
+
+router.get('/submit', function(req, res, next) {
+  res.render('upload', {title: 'New Submission'});
+})
+
+router.get('/pending', function(req, res, next) {
+  res.render('pending', {
+    title: 'Submission Pending',
+    queueNumber: 3
+  });
+})
+
+router.get('/correct', function(req, res, next) {
+  var results = [{"test1": true, "test2": true, "test3": true}];
+  res.render('judged', { title: 'Submission Evaluated',
+    correct: true,
+    results,
+    runtime: 10.3,
+    alltimes: [10.2, 12.2, 15.9, 20.2319290, 23, 50.99, 60.709, 60.709],
+    filename: "correctSubmissionPS5.java"
+  });
+});
+
+router.get('/wrong', function(req, res, next) {
+  var results = [{"test1": true, "test2": false, "test3": true}];
+  res.render('judged', { title: 'Submission Evaluated',
+    correct: false,
+    results,
+    runtime: 7.2,
+    alltimes: [10.2, 12.2, 15.9, 20.2319290, 23, 50.99, 60.709, 60.709],
+    filename: "wrongSubmissionPS5.java"
+  });
 });
 
 module.exports = router;
