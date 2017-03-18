d8ed9a73f72da19f294f4a174d7e58cd6a2be65b
Yuchuan <yyc478@gmail.com>
2017-03-18 15:25:10 +0800

    Get grader testing correctly

--- grader/grader.js
+++ grader/grader.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let bluebird = require("bluebird");
 let redis = require("redis");
 let db = redis.createClient();
@@ -7,7 +9,8 @@ let cw = require("core-worker");
 
 var queueName = "processQueue";
 
-var testcases = require('./testfiles');
+var constants = require("../constants");
+var testfiles = constants.testfiles;
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
@@ -31,21 +34,62 @@ function process(){
   db.lpopAsync(queueName)
     .then(function(filename){
       console.log(filename);
-      try {
-        var proc = cw.process(`javac -sourcepath grader/java/${filename}/ grader/java/${filename}/*.java`);
-        proc.death()
-          .then((res) => {
-            console.log(res.data);
-            console.log("completed");
-          })
-          .catch((error) => {
-            console.error(`exec error: ${error}`)
-          })
-      } catch (e) {
-        console.error(`exec error: ${e}`)
-      } finally {
-      }
+      let command = `javac -cp ./grader/java/${filename} ./grader/java/${filename}/${filename}.java`;
+      var proc = cw.process(command, /.+/);
+      proc.death()
+        .then((res) => {
+          console.log("compiled");
+          test(filename);
+        })
+        .catch((error) => {
+          console.error(`exec error: ${error}`)
+          error(filename);
+        })
     });
+}
+
+function error(filename) {
+}
+
+function test(filename) {
+  var files = Object.keys(testfiles);
+  return runTestCase(filename, files, {success:false, results:{}});
+}
+
+function runTestCase(program, files, result){
+  if(files.length == 0) {
+    result.success = true;
+    return complete(program, result);
+  }
+  let startTime = Date.now();
+  let filename = files.shift();
+  let command = `java -classpath grader/java/${program} ${program} grader/testcases/${filename}`
+  var proc = cw.process(command, /[0-9]+/);
+  proc.ready(constants.executionTimeout)
+    .then((res) => {
+      console.log(`completed ${filename}`);
+      console.log(res.result.data);
+      if(res.result.data == testfiles[filename]) {
+          result.results[filename] = Date.now() - startTime;
+          return runTestCase(program, files, result);
+      } else{
+        console.log(`${filename} incorrect ${command}`, testfiles[filename], res.result.data);
+        result.results[filename] = false;
+        return complete(program, result);
+      }
+    })
+    .catch((error) => {
+      // Kill the program if it times out
+      proc.kill();
+      console.error(`exec error for ${command}: ${error}`);
+      result.results[filename] = "Runtime Error";
+      return complete(program, result);
+    });
+}
+
+function complete(filename, results) {
+  console.log(filename);
+  console.log(results);
 }
 
 function listen(){
