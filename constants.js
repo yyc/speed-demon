98aac8ecfcbebcef9c57bfec84c2ca54f4258bf4
Yuchuan <yyc478@gmail.com>
2017-03-18 14:39:24 +0800

    Remove testfiles list and add constants.js

---
+++ constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testfiles: {
+    "2.in": 17765,
+    "QR.in":  0,
+    "example.input": 2573
+  },
+  // In milliseconds
+  executionTimeout = 20000
+}
--- grader/testfiles.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "2.in": 17765,
-  "QR.in":  0,
-  "example.input": 2573
-}
