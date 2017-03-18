41fa776fbdc4f8de1426140d323fe5879d22da1b
Yuchuan <yyc478@gmail.com>
2017-03-18 17:42:38 +0800

    UPdate with additioanl constants

--- constants.js
+++ constants.js
@@ -1,8 +1,12 @@
 module.exports = {
   testfiles: {
     "2.in": 17765,
-    "example.input": 2573
+    "example.input": 2573,
   },
   // In milliseconds
-  executionTimeout : 20000
+  executionTimeout : 20000,
+  queueName: "processQueue",
+  pubSubName: "processPubSub",
+  resultsKey: "results",
+  leaderboardKey: "leaderboard"
 }
