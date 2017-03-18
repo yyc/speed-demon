732a412dbcf0500375cb44adc1839b5e49c3afff
Yuchuan <yyc478@gmail.com>
2017-03-18 20:38:24 +0800

    Add changes to constants

--- constants.js
+++ constants.js
@@ -1,12 +1,14 @@
 module.exports = {
   testfiles: {
     "2.in": 17765,
-    "example.input": 2573,
+    "example.input": 2573
   },
   // In milliseconds
   executionTimeout : 20000,
   queueName: "processQueue",
   pubSubName: "processPubSub",
   resultsKey: "results",
-  leaderboardKey: "leaderboard"
+  leaderboardKey: "leaderboard",
+  webServerIP : "127.0.0.1",
+  redisConnectionOptions : {}
 }
