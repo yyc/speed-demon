3fabf332dedccc50bf4b85c37616985cf6514493
Yuchuan <yyc478@gmail.com>
2017-03-18 15:25:00 +0800

    Fix typo in constants

--- constants.js
+++ constants.js
@@ -1,9 +1,8 @@
 module.exports = {
   testfiles: {
     "2.in": 17765,
-    "QR.in":  0,
     "example.input": 2573
   },
   // In milliseconds
-  executionTimeout = 20000
+  executionTimeout : 20000
 }
