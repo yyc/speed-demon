9266ff3acfea3d9f42afac30fd151d488c280d75
Yuchuan <yyc478@gmail.com>
2017-03-19 22:52:09 +0800

    Add cheating to constants

--- constants.js
+++ constants.js
@@ -3,6 +3,10 @@ module.exports = {
     "2.in": 17765,
     "example.input": 2573
   },
+  // large tests, with minimum time taken. Any less and cheating is likely
+  largeTests: {
+    "2.in": 1000
+  },
   // In milliseconds
   executionTimeout : 20000,
   queueName: "processQueue",
