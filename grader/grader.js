11139d4c1208e6477289482e34fc0d360dcab172
Yuchuan <yyc478@gmail.com>
2017-10-09 11:31:43 +0800

    prettify

--- grader/grader.js
+++ grader/grader.js
@@ -5,7 +5,7 @@ let redis = require("redis");
 let Docker = require("dockerode");
 let child_process = require("child_process");
 let cw = require("core-worker");
-let fs = require('fs');
+let fs = require("fs");
 
 var constants = require("../constants");
 var testfiles = constants.testfiles;
@@ -23,16 +23,15 @@ var mutex = false;
 function start() {
   listen();
 }
-var folders = ['grader/java', 'downloads']
+var folders = ["grader/java", "downloads"];
 fs.mkdir("grader/testcases", function(err) {
   console.log(err || "testcases already exists");
 });
 
-function listen(){
-  db.llenAsync(constants.queueName)
-  .then(function(length){
-    if(length == 0) {
-      if(sub.listenerCount("message") == 0){
+function listen() {
+  db.llenAsync(constants.queueName).then(function(length) {
+    if (length == 0) {
+      if (sub.listenerCount("message") == 0) {
         console.log("waiting");
         sub.once("message", (channel, message) => {
           process();
@@ -44,132 +43,155 @@ function listen(){
   });
 }
 
-
-function process(){
-  db.lpopAsync(constants.queueName)
-    .then(function(filedata){
-      if(filedata == null) {
-        return listen();
-      }
-      filedata = JSON.parse(filedata);
-      let classname = filedata.classname;
-      let folderkey = filedata.key
-      console.log(filedata);
-      if(filedata.type == "zip") {
-        var dl = cw.process(`wget -q -P downloads ${constants.webServerIP}/uploads/${folderkey}`).death()
-          .catch((e) => {
-            console.error("Download error");
-            error(filedata, "Download Error");
-          })
-          .then((res) => {
-            return cw.process(`unzip -j -o -d grader/java/${folderkey} downloads/${folderkey}`).death()
-          });
-      } else {
-        var dl = cw.process(`mkdir grader/java/${folderkey}`).death()
-          .then(() => cw.process(`wget -O grader/java/${folderkey}/${classname}.java ${constants.webServerIP}/uploads/${folderkey}`).death());
-      }
-      var compileProcess;
-      dl.catch((e) => {
+function process() {
+  db.lpopAsync(constants.queueName).then(function(filedata) {
+    if (filedata == null) {
+      return listen();
+    }
+    filedata = JSON.parse(filedata);
+    let classname = filedata.classname;
+    let folderkey = filedata.key;
+    console.log(filedata);
+    if (filedata.type == "zip") {
+      var dl = cw
+        .process(
+          `wget -q -P downloads ${constants.webServerIP}/uploads/${folderkey}`
+        )
+        .death()
+        .catch(e => {
+          console.error("Download error");
+          error(filedata, "Download Error");
+        })
+        .then(res => {
+          return cw
+            .process(
+              `unzip -j -o -d grader/java/${folderkey} downloads/${folderkey}`
+            )
+            .death();
+        });
+    } else {
+      var dl = cw
+        .process(`mkdir grader/java/${folderkey}`)
+        .death()
+        .then(() =>
+          cw
+            .process(
+              `wget -O grader/java/${folderkey}/${classname}.java ${constants.webServerIP}/uploads/${folderkey}`
+            )
+            .death()
+        );
+    }
+    var compileProcess;
+    dl
+      .catch(e => {
         console.error(e);
         error(filedata, "Download Error");
       })
-        .then(() => {
-          let command = `javac -cp ./grader/java/${folderkey} ./grader/java/${folderkey}/${classname}.java`;
-          console.log(command);
-          compileProcess = cw.process(command, /.+/);
-          return compileProcess.death()
-        })
-        .then((res) => {
-          test(filedata);
-        })
-        .catch((e) => {
-          error(filedata, "Compilation Error");
-          var output = compileProcess.instance.instance.output.join('').replace(/<error>/g,'');
-          error(filedata, output);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
-    });
+      .then(() => {
+        let command = `javac -cp ./grader/java/${folderkey} ./grader/java/${folderkey}/${classname}.java`;
+        console.log(command);
+        compileProcess = cw.process(command, /.+/);
+        return compileProcess.death();
+      })
+      .then(res => {
+        test(filedata);
+      })
+      .catch(e => {
+        error(filedata, "Compilation Error");
+        var output = compileProcess.instance.instance.output
+          .join("")
+          .replace(/<error>/g, "");
+        error(filedata, output);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  });
 }
 
 function error(filedata, err) {
-  return complete(filedata, {success:false, results:{}, compileError: err})
+  return complete(filedata, { success: false, results: {}, compileError: err });
 }
 
 function test(filedata) {
   var files = Object.keys(testfiles);
-  return runTestCase(filedata, files, {success:false, results:{}});
+  return runTestCase(filedata, files, { success: false, results: {} });
 }
 
-function runTestCase(filedata, files, result){
-  if(files.length == 0) {
+function runTestCase(filedata, files, result) {
+  if (files.length == 0) {
     result.success = true;
     try {
       return complete(filedata, result);
-    } catch(e) {
+    } catch (e) {
       console.error(e);
     }
     return;
   }
   let startTime = Date.now();
   let filename = files.shift();
-  let command = `java -classpath grader/java/${filedata.key} -Xmx1500m ${filedata.classname} grader/testcases/${filename}`
+  let command = `java -classpath grader/java/${filedata.key} -Xmx1500m ${filedata.classname} grader/testcases/${filename}`;
   var proc = cw.process(command, /[0-9]+/);
-  proc.ready(constants.executionTimeout)
-    .then((res) => {
-      var output = proc.instance.instance.output.join('');
+  proc
+    .ready(constants.executionTimeout)
+    .then(res => {
+      var output = proc.instance.instance.output.join("");
       console.log(`completed ${filename} ${output}`);
-      if(output == testfiles[filename]) {
-          let runtime = Date.now() - startTime;
-          if(constants.largeTests[filename] && runtime < constants.largeTests[filename]) {
-            result.results = [];
-            result.runtimeError = "CHEATING DETECTED\nHardcoding is not appreciated >:(";
-            db.zadd(constants.cheatersKey, runtime, filedata.name);
-            return complete(filedata, result);
-          }
-          result.results[filename] = runtime;
-          return runTestCase(filedata, files, result);
-      } else{
+      if (output == testfiles[filename]) {
+        let runtime = Date.now() - startTime;
+        if (
+          constants.largeTests[filename] &&
+          runtime < constants.largeTests[filename]
+        ) {
+          result.results = [];
+          result.runtimeError =
+            "CHEATING DETECTED\nHardcoding is not appreciated >:(";
+          db.zadd(constants.cheatersKey, runtime, filedata.name);
+          return complete(filedata, result);
+        }
+        result.results[filename] = runtime;
+        return runTestCase(filedata, files, result);
+      } else {
         // console.log(`${filename} incorrect`, testfiles[filename], res.result.data);
         result.results[filename] = false;
-        if(output.includes("<error>")) {
+        if (output.includes("<error>")) {
           console.log(output);
-          result.runtimeError = output.replace(/<error>/g,'');
+          result.runtimeError = output.replace(/<error>/g, "");
         } else {
           result.runtimeError = "Wrong Answer";
         }
         return complete(filedata, result);
       }
     })
-    .catch((error) => {
+    .catch(error => {
       // Kill the program if it times out
       result.results[filename] = false;
-      if(Date.now() - startTime > constants.executionTimeout) {
+      if (Date.now() - startTime > constants.executionTimeout) {
         result.runtimeError = "Time Limit Exceeded";
-      } else{
+      } else {
         console.error(`exec error for ${command}: ${error}`);
         result.runtimeError = proc.instance.instance.output;
       }
       return complete(filedata, result);
     });
-    setTimeout(() => {
-      if(proc.instance.instance.isRunning){
-        proc.kill();
-      }
-    }, constants.executionTimeout)
-
+  setTimeout(() => {
+    if (proc.instance.instance.isRunning) {
+      proc.kill();
+    }
+  }, constants.executionTimeout);
 }
 
 function complete(filedata, results) {
-  if(results.success) {
-    var time = Object.values(results.results)
-      .reduce((acc, value) => acc + value, 0);
+  if (results.success) {
+    var time = Object.values(results.results).reduce(
+      (acc, value) => acc + value,
+      0
+    );
     results.time = time;
   }
   console.log(constants.resultsKey, filedata.key, JSON.stringify(results));
   db.hset(constants.resultsKey, filedata.key, JSON.stringify(results));
-  if(results.success) {
+  if (results.success) {
     console.log(constants.leaderboardKey, time, filedata.name);
     db.zadd(constants.leaderboardKey, time, filedata.name);
   }
--- grader/localGrader.js
+++ grader/localGrader.js
@@ -6,7 +6,6 @@ let Docker = require("dockerode");
 let child_process = require("child_process");
 let cw = require("core-worker");
 
-
 var constants = require("../constants");
 var testfiles = constants.testfiles;
 
@@ -18,21 +17,20 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 var mutex = false;
-var list = require("./localFiles")
+var list = require("./localFiles");
 // Processes any pending jobs in the queue,
 // or waits for a pubsub
 function start() {
   listen();
 }
 
-function listen(){
-  if(list.length > 0) {
+function listen() {
+  if (list.length > 0) {
     process();
   }
 }
 
-
-function process(){
+function process() {
   var tuple = list.pop();
   var folderKey = tuple[0];
   var classname = tuple[1];
@@ -40,67 +38,75 @@ function process(){
     key: folderKey,
     name: folderKey,
     classname
-  }
+  };
   let command = `javac -cp ./grader/java/${folderKey} ./grader/java/${folderKey}/${classname}.java`;
-    console.log(command);
-    var compileProcess = cw.process(command, /.+/);
-    compileProcess.death()
-        .then((res) => {
-          test(filedata);
-        })
-        .catch((e) => {
-          error(filedata, "Compilation Error");
-          var output = compileProcess.instance.instance.output.join('').replace(/<error>/g,'');
-          error(filedata, output);
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+  console.log(command);
+  var compileProcess = cw.process(command, /.+/);
+  compileProcess
+    .death()
+    .then(res => {
+      test(filedata);
+    })
+    .catch(e => {
+      error(filedata, "Compilation Error");
+      var output = compileProcess.instance.instance.output
+        .join("")
+        .replace(/<error>/g, "");
+      error(filedata, output);
+    })
+    .catch(e => {
+      console.log(e);
+    });
 }
 
 function error(filedata, err) {
-  return complete(filedata, {success:false, results:{}, compileError: err})
+  return complete(filedata, { success: false, results: {}, compileError: err });
 }
 
 function test(filedata) {
   var files = Object.keys(testfiles);
-  return runTestCase(filedata, files, {success:false, results:{}});
+  return runTestCase(filedata, files, { success: false, results: {} });
 }
 
-function runTestCase(filedata, files, result){
-  if(files.length == 0) {
+function runTestCase(filedata, files, result) {
+  if (files.length == 0) {
     result.success = true;
     try {
       return complete(filedata, result);
-    } catch(e) {
+    } catch (e) {
       console.error(e);
     }
     return;
   }
   let startTime = Date.now();
   let filename = files.shift();
-  let command = `java -classpath grader/java/${filedata.key} -Xmx1500m ${filedata.classname} grader/testcases/${filename}`
+  let command = `java -classpath grader/java/${filedata.key} -Xmx1500m ${filedata.classname} grader/testcases/${filename}`;
   var proc = cw.process(command, /[0-9]+/);
-  proc.ready(constants.executionTimeout)
-    .then((res) => {
-      var output = proc.instance.instance.output.join('');
+  proc
+    .ready(constants.executionTimeout)
+    .then(res => {
+      var output = proc.instance.instance.output.join("");
       console.log(`completed ${filename} ${output}`);
-      if(output == testfiles[filename]) {
-          let runtime = Date.now() - startTime;
-          if(constants.largeTests[filename] && runtime < constants.largeTests[filename]) {
-            result.results = [];
-            result.runtimeError = "CHEATING DETECTED\nHardcoding is not appreciated >:(";
-            db.zadd(constants.cheatersKey, runtime, filedata.name);
-            return complete(filedata, result);
-          }
-          result.results[filename] = runtime;
-          return runTestCase(filedata, files, result);
-      } else{
+      if (output == testfiles[filename]) {
+        let runtime = Date.now() - startTime;
+        if (
+          constants.largeTests[filename] &&
+          runtime < constants.largeTests[filename]
+        ) {
+          result.results = [];
+          result.runtimeError =
+            "CHEATING DETECTED\nHardcoding is not appreciated >:(";
+          db.zadd(constants.cheatersKey, runtime, filedata.name);
+          return complete(filedata, result);
+        }
+        result.results[filename] = runtime;
+        return runTestCase(filedata, files, result);
+      } else {
         // console.log(`${filename} incorrect`, testfiles[filename], res.result.data);
         result.results[filename] = false;
-        if(output.includes("<error>")) {
+        if (output.includes("<error>")) {
           console.log(output);
-          result.runtimeError = output.replace(/<error>/g,'');
+          result.runtimeError = output.replace(/<error>/g, "");
         } else {
           result.runtimeError = "Wrong Answer";
         }
@@ -109,34 +115,35 @@ function runTestCase(filedata, files, result){
         return runTestCase(filedata, files, result);
       }
     })
-    .catch((error) => {
+    .catch(error => {
       // Kill the program if it times out
       result.results[filename] = false;
-      if(Date.now() - startTime > constants.executionTimeout) {
+      if (Date.now() - startTime > constants.executionTimeout) {
         result.runtimeError = "Time Limit Exceeded";
-      } else{
+      } else {
         console.error(`exec error for ${command}: ${error}`);
         result.runtimeError = proc.instance.instance.output;
       }
       return complete(filedata, result);
     });
-    setTimeout(() => {
-      if(proc.instance.instance.isRunning){
-        proc.kill();
-      }
-    }, constants.executionTimeout)
-
+  setTimeout(() => {
+    if (proc.instance.instance.isRunning) {
+      proc.kill();
+    }
+  }, constants.executionTimeout);
 }
 
 function complete(filedata, results) {
-  if(results.success) {
-    var time = Object.values(results.results)
-      .reduce((acc, value) => acc + value, 0);
+  if (results.success) {
+    var time = Object.values(results.results).reduce(
+      (acc, value) => acc + value,
+      0
+    );
     results.time = time;
   }
   console.log(constants.resultsKey, filedata.key, JSON.stringify(results));
   db.hset(constants.resultsKey, filedata.key, JSON.stringify(results));
-  if(results.success) {
+  if (results.success) {
     console.log(constants.leaderboardKey, time, filedata.name);
     db.zadd(constants.leaderboardKey, time, filedata.name);
   }
