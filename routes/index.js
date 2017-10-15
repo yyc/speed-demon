4dcff127ff76b5dbf1b94572cd2b64deb1ad3097
Yuchuan <yyc478@gmail.com>
2017-10-15 16:04:56 +0800

    remove log statement

--- routes/index.js
+++ routes/index.js
@@ -79,7 +79,6 @@ router.post("/submit", function(req, res, next) {
     data.type = "zip";
   } else if (req.files.file.name.includes(".java")) data.type = "java";
   else {
-    console.log(req.files.file);
     return res.render("upload", {
       title: "New Submission",
       error: "Invalid filetype"
