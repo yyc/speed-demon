0c1fbda8af882f4092510f5da9c4bf9a6798c62a
Yuan Yuchuan <yuan.yuchuan@affirm.com>
2020-03-22 14:13:41 -0400

    run local code in docker for better isolation

--- grader/grader.js
+++ grader/grader.js
@@ -5,8 +5,8 @@ let redis = require("redis");
 let cw = require("core-worker");
 let fs = require("fs");
 
-var constants = require("../constants");
-var testfiles = constants.testfiles;
+const constants = require("../constants");
+const testfiles = constants.testfiles;
 
 let db = redis.createClient(constants.redisConnectionOptions);
 let sub = redis.createClient(constants.redisConnectionOptions);
@@ -14,6 +14,8 @@ sub.subscribe(constants.pubSubName);
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
+
+const cwd = process.cwd();
 
 // Processes any pending jobs in the queue,
 // or waits for a pubsub
@@ -40,13 +42,12 @@ async function listen() {
     let length = await db.llenAsync(constants.queueName);
     if (length == 0) {
       await messagePromise();
-    } else {
-      await process();
     }
+    await processTestCase();
   }
 }
 
-async function process() {
+async function processTestCase() {
   let filedata_str = await db.lpopAsync(constants.queueName);
 
   // someone beat us to the punch
@@ -84,8 +85,10 @@ async function process() {
   }
   let compileProcess;
   try {
-    let command = `javac -cp ./grader/java/${folderkey} ./grader/java/${folderkey}/${classname}.java`;
-    compileProcess = cw.process(command, /.+/);
+    compileProcess = getDockerProcess(
+      `javac -cp /submission ./submission/${classname}.java`,
+      [`grader/java/${folderkey}:/submission`]
+    );
     await compileProcess.death();
   } catch (e) {
     console.log();
@@ -119,9 +122,15 @@ async function runTestCase(filedata, files, result) {
   }
   let startTime = Date.now();
   let filename = files.shift();
-  let command = `java -classpath grader/java/${filedata.key} -Xmx1500m ${filedata.classname} grader/testcases/${filename}`;
-  console.log(command);
-  var proc = cw.process(command, /[0-9]+/);
+  let proc = getDockerProcess(
+    `java -classpath /submission -Xmx1500m ${filedata.classname} /testfile`,
+    [
+      `grader/java/${filedata.key}:/submission`,
+      // mount as readonly, so people can't do funny things like modify the input file
+      // Also, only mount the input file so they can't cheat by looking for the .out file
+      `grader/testcases/${filename}:/testfile:ro`
+    ]
+  );
 
   setTimeout(() => {
     if (proc.instance.instance.isRunning) {
@@ -204,6 +213,23 @@ async function complete(filedata, results) {
     promises.push(db.zaddAsync(constants.leaderboardKey, time, name));
   }
   await Promise.all(promises);
+}
+
+/* 
+Execute the java code in a docker container. This gives us several advantages:
+1. Isolating the code, so they can't mess up the machine we're running the grader on
+  NB: if they manage to escape the container, just give them a medal and ask them to use their energy more productively elsewhere
+2. Allows us to mount just the test input file in readonly mode, so they can't modify it or look for the corresponding .out file
+3. Restricts networking, so they can't send the input file to themselves.
+4. More portable, since we don't have to mess with a possibly existing java installation. 
+*/
+function getDockerProcess(command, volumes = []) {
+  let docker_command = ["docker run --rm --network none"];
+  docker_command.push(...volumes.map(vol => `-v ${cwd}/${vol}`));
+  docker_command.push("openjdk:12");
+  docker_command.push(command);
+  console.log(docker_command.join(" "));
+  return cw.process(docker_command.join(" "));
 }
 
 start();
